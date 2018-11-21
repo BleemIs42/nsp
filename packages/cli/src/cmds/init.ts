04c5a2089b9f5332fb972ea6f211511138e4e78f
BarryYan <barry.yansh@gmail.com>
2018-11-21 16:46:16 +0800

    ✨ (cli): addd

--- packages/cli/src/cmds/init.ts
+++ packages/cli/src/cmds/init.ts
@@ -39,7 +39,7 @@ class Init implements Interfaces.Cli {
   public get builder() {
     return {
       dir: {
-        default: 'init dir test'
+        default: 'init a dir test'
       }
     }
   }
